726197e0705c3e2b39bd420b8e685478e6fa4ad7
이승민 <sml7179@naver.com>
2022-03-20 15:01:34 +0900

    ch14_15_16 setting

---
+++ ch14_15_16/src/main/java/com/example/ch14_15_16/Ch141516Application.java
@@ -0,0 +1,13 @@
+package com.example.ch14_15_16;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch141516Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ch141516Application.class, args);
+    }
+
+}
---
+++ ch14_15_16/src/test/java/com/example/ch14_15_16/Ch141516ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ch14_15_16;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ch141516ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
